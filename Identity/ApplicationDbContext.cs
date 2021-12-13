b054aba717418b587785a083fd178960cc9e1d3a
John Hacker <Yohannes.Kindu@gmail.com>
2021-12-13 23:05:49 +0300

    Add Basic Identity functions, added full name attribute to the table, and migration done.

---
+++ Identity/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCCS_School_Parent_Communication_System.Identity
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("spcsconn")
+        {
+
+        }
+    }
+}
---
+++ Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace LCCS_School_Parent_Communication_System.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string fullName { get; set; }
+    }
+}
